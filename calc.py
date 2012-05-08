6177916ec194643b6f6411f5900f894ae8ff2e6f
C. Titus Brown <titus@idyll.org>
2012-05-07 22:40:15 -0400

    initial foo

---
+++ calc.py
@@ -0,0 +1,16 @@
+import sys
+
+x = []
+for line in open(sys.argv[1]):
+   num = int(line)
+   x.append(num)
+
+avg = sum(x) / float(len(x))
+
+diffs = []
+for num in x:
+   diff = num - avg
+   diffs.append(diff**2)
+
+print 'average is', sum(x) / float(len(x))
+#print 'sumsqdiffs is', sum(diffs)
